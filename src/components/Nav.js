a56a2fe4e0267f97138ef40b525df1af847c2126
Robert Allen <RALLEN6@STEDWARDS.EDU>
2020-03-27 12:04:45 -0500

    Moved Social Media icons to Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,8 +23,15 @@ function Nav() {
       <Row>
         <Col xs={3} md={6}>
           <a href="http://linkedin.com/in/robert-allen-jr-11183382">
-          <img src= "https://img.icons8.com/ios/75/000000/linkedin.png" align = "left"  width="50px" height="50px" className="d-inline-block align-top" display= "flex" padding-left= "20px" alt= "50x50 "/>
+          <img src= "https://img.icons8.com/ios/75/000000/linkedin.png" align = "left"  width="45px" height="45px" className="d-inline-block align-top" display= "flex" padding-left= "20px" alt= "50x50 "/>
         </a>
+        <nav id="colorlib-main-menu">
+                        <ul>
+                            <li><a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook"/></a></li>
+                            <li><a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"/></a></li>
+                            <li><a href="https://bitbucket.org/" target="_blank" rel="noopener noreferrer"><i className="fa fa-bitbucket"/></a></li>
+                        </ul>
+                    </nav>
         </Col>
         <Col xs={3} md={2}>
         <Link to = "/" style={{ textDecoration: 'none', fontWeight: "bold", align: "right" }} className = "link"> 
