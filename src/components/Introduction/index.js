fafd7c3d712bcfa35e5438230f56731e204cbd42
Robert Allen <RALLEN6@STEDWARDS.EDU>
2021-01-06 02:17:57 -0600

    added resume button, reconfigured layout

---
+++ src/components/Introduction/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Introduction() {
+    return (
+      <introduction>
+        <div>
+          <section id="colorlib-hero" className="js-fullheight" data-section="home">
+                <div className="flex-container">
+              <div className="overlay" />
+                <section className="resume">
+                    <div className="desc">
+                      <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/1ddt-E7YW0sH6_kqMpaoEy4HJNwgIerh2/view?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="fa fa-linkedin" /></a></p>
+                    </div>
+                </section>
+              </div>   
+          </section>
+        </div>
+      </introduction>
+    );
+  }
+export default Introduction;
---
+++ src/components/Resume/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Document, Page } from 'react-pdf/dist/esm/entry.parcel';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+
+import pdfFile from "https://drive.google.com/file/d/1ddt-E7YW0sH6_kqMpaoEy4HJNwgIerh2/view?usp=sharing";
+
+const options = {
+  cMapUrl: 'cmaps/',
+  cMapPacked: true,
+};
+
+export default function Resume() {
+  const [file, setFile] = useState(pdfFile);
+  const [numPages, setNumPages] = useState(null);
+
+  function onFileChange(event) {
+    setFile(event.target.files[0]);
+  }
+
+  function onDocumentLoadSuccess({ numPages: nextNumPages }) {
+    setNumPages(nextNumPages);
+  }
+
+  return (
+    <div className="Example">
+      <header>
+        <h1>react-pdf sample page</h1>
+      </header>
+      <div className="Example__container">
+        <div className="Example__container__load">
+          <label htmlFor="file">Load from file:</label>
+          {' '}
+          <input
+            onChange={onFileChange}
+            type="file"
+          />
+        </div>
+        <div className="Example__container__document">
+          <Document
+            file={file}
+            onLoadSuccess={onDocumentLoadSuccess}
+            options={options}
+          >
+            {
+              Array.from(
+                new Array(numPages),
+                (el, index) => (
+                  <Page
+                    key={`page_${index + 1}`}
+                    pageNumber={index + 1}
+                  />
+                ),
+              )
+            }
+          </Document>
+        </div>
+      </div>
+    </div>
+  );
+}
