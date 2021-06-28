2fb54a5f723c823a7c70b6a1c4dd54aba5d07d04
Robert Allen <rallen186@outlook.com>
2021-06-28 12:41:09 -0500

    Deleted unused card and image code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -10,7 +10,6 @@ import FTyping from "./FormTypewriter";
 import homeLogo from "../../images/HomePageJourney.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { motion } from "framer-motion";
-import { useViewportScroll } from "framer-motion";
 import { AnimatePresence } from "framer-motion";
 import AnimateComponent from "./AnimateOnScroll";
 
@@ -72,7 +71,7 @@ export default class Home extends React.Component {
         <motion.div className="homeProject"
         onHoverStart={() => console.log("Hover ends ")}
         onHoverStart={() => this.setState({isToggled: true})}
-        onHoverEnd={() => this.setState({isToggled: false})}
+        onHoverEnd={() => this.setState({isToggled: true})}
         >
         <AnimatePresence>
           {this.state.isToggled === true && (
@@ -133,59 +132,3 @@ export default class Home extends React.Component {
     );
   }
 }
-
-
-function Image({ ratio, src }) {
-  return (
-    <div className="image-container">
-      <div className="image-inner-container">
-        <div
-          className="ratio"
-          style={{
-            paddingTop: ratio * 100 + '%'
-          }}
-        >
-          <div className="ratio-inner">
-            <img src={src} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function Info() {
-  return (
-    <div className="info">
-      Springy cards from{' '}
-      <a target="_blank" href="https://bit.ly/382KSdo">
-        divjoy.com
-      </a>
-      <div className="notice">(best viewed at larger screen width)</div>
-    </div>
-  );
-}
-
-const cards = [
-  {
-    title: 'Build faster ⚡️',
-    description:
-      'Create a React web app in the fraction of the time using our library of themes and building blocks. We have everything from navbars and content grids to authentication flows and commenting systems. New blocks are added every week.',
-    image: 'https://6jlvz1j5q3.csb.app/undraw_collection.svg',
-    imageRatio: 784 / 1016
-  },
-  {
-    title: 'Tweak anything 👩‍🎨',
-    description:
-      'Built with developers in mind. Change element structure, edit CSS, create components, add props and state. We give you access to the underlying React code so you can do what you need right in our tool.',
-    image: 'https://6jlvz1j5q3.csb.app/undraw_upload.svg',
-    imageRatio: 839 / 1133
-  },
-  {
-    title: 'Export your code 🚀',
-    description:
-      "Export your project as a high-quality React codebase. We're lazer focused on helping you build and iterate quickly, but expect that you'll eventually want to export and wrap things up in your favorite code editor.",
-    image: 'https://6jlvz1j5q3.csb.app/undraw_static_assets.svg',
-    imageRatio: 730 / 1030
-  }
-];
