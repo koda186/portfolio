39ef38b803d5ce12e7f3344b12fdfc3acc6b79ed
Robert Allen <rallen186@outlook.com>
2021-07-19 15:10:37 -0500

    Added the RaspberryPi component and path for future site. Code for completed IOT project is in Bitbucket repo.

---
+++ src/components/RaspPI/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function RaspPI(props) {
+  return (
+    <div className="flex-container">
+      <h1><br />Internet Of Things</h1>
+      <h3>For now check out the project repo on Bitbucket under <br />~/SWE2 Raspberry PI IOT-- Shelf Space Analysis</h3>
+      <h4>More Coming Soon!</h4>
+    </div>
+  );
+}
+
+export default RaspPI;
