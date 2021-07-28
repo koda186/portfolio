693b1e99758eb524464dcb828840a79d7db03abb
Robert Allen <rallen186@outlook.com>
2021-07-28 08:34:31 -0500

    Implemented Arduino Page with basic parallax functionality

--- src/components/Arduino/index.js
+++ src/components/Arduino/index.js
@@ -1,11 +1,18 @@
 import React from "react"
+import "./index.css";
+import ParallaxJSXWrapper from "../ParallaxJSXWrapper";
 
 function Arduino(props) {
+
+    const Section1 = ParallaxJSXWrapper(
+      <div className="flex-container">
+        <h1><br />Smart Plant</h1>
+        <h3>Coming Soon!</h3>
+      </div>
+      ,1.10);
   return (
-    <div className="flex-container">
-      <h1><br />Smart Plant</h1>
-      <h3>Coming Soon!</h3>
-    </div>
+      <Section1 />
+
   );
 }
 
