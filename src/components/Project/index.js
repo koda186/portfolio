3516cfc5ba43f42bd1d6aefc18d7618bebd3fc0a
Robert Allen <rallen186@outlook.com>
2021-09-10 14:17:27 -0500

    Add comments back to the code

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -8,13 +8,15 @@ import SystemDiagram from './images/SystemDiagram.png'
 import { Col } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import {Container} from 'react-bootstrap';
-import ReactCursorPosition from 'react-cursor-position';
-import PositionLabel from "../ComponentToTrack";
 import "./index.css";
 
 
-//Projects component
+//Projects component - Conceptually, components are like JavaScript functions.
+//They accept arbitrary inputs (called “props”) and return React elements
+//describing what should appear on the screen.
 //The data we pass into the thumbnails are the props
+//Two ways to define a component, a function or ES6 class
+
 function Projects(props) {
   return (
 
@@ -26,44 +28,35 @@ function Projects(props) {
         {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
         <Row>
           <Col xs={12} md={6}>
-            <ReactCursorPosition className="example">
+            {/*When React sees an element representing a user-defined component,
+            it passes JSX attributes and children to this component as a
+            single object. We call this object “props”.*/}
             <Thumbnail
             link = "/raspberryPi"
             image= {raspberryPi} alt= "100x100 "
             />
-            <PositionLabel />
-            </ReactCursorPosition>
           </Col>
 
           <Col xs={12} md={6}>
-            <ReactCursorPosition className="example">
             <Thumbnail
             link = "/arduino"
             image= {arduino} alt= "100x100 "
             />
-            <PositionLabel />
-            </ReactCursorPosition>
           </Col>
         </Row>
 
         <Row>
           <Col xs={12} md={6}>
-            <ReactCursorPosition className="example">
             <Thumbnail
             link = "/"
             image= {SeniorProject} alt= "100x100 "
             />
-            <PositionLabel />
-            </ReactCursorPosition>
           </Col>
           <Col xs={12} md={6}>
-            <ReactCursorPosition className="example">
             <Thumbnail
             link = "/SystemDiagram"
             image= {SystemDiagram} alt= "100x100 "
             />
-            <PositionLabel />
-            </ReactCursorPosition>
           </Col>
         </Row>
 
